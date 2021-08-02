5c718bad2438a5bb0883d29fc5ab4865a8202532
Raul Melo <melo.raulf@gmail.com>
2021-08-02 08:13:07 +0200

    chore: change to be static output

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 /** @type {import('@sveltejs/kit').Config} */
-
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    adapter: vercel(),
-    ssr: false
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    })
   }
 };
