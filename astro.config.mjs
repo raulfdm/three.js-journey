64cc4ecec6eaa8c6d93ad5f75037379e722d3bb9
Raul Melo <melo.raulf@gmail.com>
2023-09-24 13:59:22 +0200

    ignore history folder from dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    server: {
+      watch: {
+        ignored: ["**/.history/**/*"],
+      },
+    },
+  },
 });
