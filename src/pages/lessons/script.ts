360340e09096ce78af3b61952a01eb02a0eed706
Raul Melo <melo.raulf@gmail.com>
2023-09-24 12:24:49 +0200

    prevent SSR build

--- src/pages/lessons/script.ts
+++ src/pages/lessons/script.ts
@@ -1,34 +1,40 @@
 import * as THREE from "three";
 
-const scene = new THREE.Scene();
+if (typeof window !== "undefined") {
+  render();
+}
 
-const redCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const redMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+function render() {
+  const scene = new THREE.Scene();
 
-const cube = new THREE.Mesh(redCubeGeometry, redMaterial);
+  const redCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const redMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 
-// We need to add to the scene
-scene.add(cube);
+  const cube = new THREE.Mesh(redCubeGeometry, redMaterial);
 
-const sizes = {
-  width: 800,
-  height: 600,
-  get aspectRatio() {
-    return sizes.width / sizes.height;
-  },
-};
+  // We need to add to the scene
+  scene.add(cube);
 
-const camera = new THREE.PerspectiveCamera(75, sizes.aspectRatio);
-camera.position.z = 3;
+  const sizes = {
+    width: 800,
+    height: 600,
+    get aspectRatio() {
+      return sizes.width / sizes.height;
+    },
+  };
 
-// We need to add a camera
-scene.add(camera);
+  const camera = new THREE.PerspectiveCamera(75, sizes.aspectRatio);
+  camera.position.z = 3;
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("canvas") as HTMLCanvasElement,
-});
+  // We need to add a camera
+  scene.add(camera);
 
-renderer.setSize(sizes.width, sizes.height);
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("canvas") as HTMLCanvasElement,
+  });
 
-// Render things in the canvas
-renderer.render(scene, camera);
+  renderer.setSize(sizes.width, sizes.height);
+
+  // Render things in the canvas
+  renderer.render(scene, camera);
+}
