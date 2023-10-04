638926c1c34af438b73667a65fcf9343bcfbf83a
Raul Melo <melo.raulf@gmail.com>
2023-10-04 19:08:17 +0200

    finish lesson 13

--- src/pages/lessons/13/script.ts
+++ src/pages/lessons/13/script.ts
@@ -1,6 +1,10 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Font } from "three/addons/loaders/FontLoader.js";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 import GUI from "lil-gui";
+import helvetikerRegularFont from "three/examples/fonts/helvetiker_regular.typeface.json";
+import matCapImage from "@/assets/textures/matcaps/10.png";
 
 if (typeof window !== "undefined") {
   render();
@@ -46,10 +50,63 @@ function render() {
   });
 
   const scene = new THREE.Scene();
+  const loadedFont = new Font(helvetikerRegularFont);
+  const textureLoader = new THREE.TextureLoader();
+  const matCapTexture = textureLoader.load(matCapImage.src);
 
-  const material = new THREE.MeshBasicMaterial();
+  const text = new TextGeometry("Hello Three.JS", {
+    font: loadedFont,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 5,
+    /**
+     * Bevel
+     */
+    bevelEnabled: true, // must be on so it can bevel
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelSegments: 4,
+  });
 
-  const box = new THREE.Mesh(new THREE.BoxGeometry(), material);
+  text.center();
+
+  // text.computeBoundingBox();
+
+  // text.translate(
+  //   -(text.boundingBox!.max.x - 0.02) * 0.5,
+  //   -(text.boundingBox!.max.y - 0.02) * 0.5,
+  //   -(text.boundingBox!.max.z - 0.03) * 0.5
+  // );
+
+  // text.computeBoundingBox();
+  // console.log(text.boundingBox);
+
+  const material = new THREE.MeshMatcapMaterial();
+  material.matcap = matCapTexture;
+
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  const donutMaterial = new THREE.MeshMatcapMaterial();
+  donutMaterial.matcap = matCapTexture;
+
+  console.time("donuts");
+  for (let numberOfDonuts = 0; numberOfDonuts < 100; numberOfDonuts++) {
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+
+    donut.position.x = (Math.random() - 0.5) * 6;
+    donut.position.y = (Math.random() - 0.5) * 6;
+    donut.position.z = (Math.random() - 0.5) * 6;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+
+    scene.add(donut);
+  }
+  console.timeEnd("donuts");
+
+  const box = new THREE.Mesh(text, material);
 
   scene.add(box);
 
