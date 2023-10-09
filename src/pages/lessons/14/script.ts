124f2d658a7eda3244f4f4f91f7e1010e3085e14
Raul de Melo <melo.raulf@gmail.com>
2023-10-09 09:51:08 -0300

    add lesson 14

---
+++ src/pages/lessons/14/script.ts
@@ -0,0 +1,106 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI from "lil-gui";
+
+if (typeof window !== "undefined") {
+  render();
+}
+
+function render() {
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+
+  if (!canvas) {
+    throw new Error("No canvas found");
+  }
+
+  const gui = new GUI();
+
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    get aspectRatio() {
+      return sizes.width / sizes.height;
+    },
+  };
+
+  window.addEventListener("resize", () => {
+    // Update the sizes
+    sizes.height = window.innerHeight;
+    sizes.width = window.innerWidth;
+
+    // Update the camera
+    camera.aspect = sizes.aspectRatio;
+    camera.updateProjectionMatrix();
+
+    // Update the renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  window.addEventListener("dblclick", () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      canvas.requestFullscreen();
+    }
+  });
+
+  const scene = new THREE.Scene();
+
+  /**
+   * Lights
+   */
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+
+  const material = new THREE.MeshStandardMaterial();
+
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.6, 32, 16),
+    material
+  );
+  sphere.position.x = -1.5;
+  scene.add(sphere);
+
+  const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+  scene.add(box);
+
+  const donut = new THREE.Mesh(
+    new THREE.TorusGeometry(0.4, 0.3, 16, 64),
+    material
+  );
+  donut.position.x = 1.5;
+  scene.add(donut);
+
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+  plane.rotation.x = -Math.PI * 0.5;
+  plane.position.y = -0.65;
+  scene.add(plane);
+
+  const camera = new THREE.PerspectiveCamera(45, sizes.aspectRatio);
+  camera.position.x = 1;
+  camera.position.y = 1;
+  camera.position.z = 2;
+
+  scene.add(camera);
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+  });
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  function tick() {
+    controls.update();
+
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+  }
+
+  tick();
+}
