ca6a270f04446f8250bb66f831b3c3ad3e3558e2
Raul de Melo <melo.raulf@gmail.com>
2023-10-29 12:21:34 -0300

    add basic 18 lesson code

---
+++ src/pages/lessons/18/script.ts
@@ -0,0 +1,118 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI from "lil-gui";
+
+if (typeof window !== "undefined") {
+  render();
+}
+
+function render() {
+  THREE.ColorManagement.enabled = false;
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+
+  if (!canvas) {
+    throw new Error("No canvas found");
+  }
+
+  const gui = new GUI();
+
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    get aspectRatio() {
+      return sizes.width / sizes.height;
+    },
+  };
+
+  const scene = new THREE.Scene();
+
+  setResizeEventListeners();
+
+  const textures = createTextures();
+  const lights = createLights();
+  const geometries = createGeometries();
+  const camera = createCamera();
+  const controls = createControls();
+  const renderer = createRenderer();
+  const clock = new THREE.Clock();
+
+  updateRenderer();
+
+  /**
+   * Methods
+   */
+
+  function updateRenderer() {
+    const elapsedTime = clock.getElapsedTime();
+
+    controls.update();
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(updateRenderer);
+  }
+
+  function createCamera() {
+    const camera = new THREE.PerspectiveCamera(75, sizes.aspectRatio, 0.1, 100);
+    camera.position.z = 3;
+    scene.add(camera);
+
+    return camera;
+  }
+
+  function createControls() {
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+
+    return controls;
+  }
+
+  function createLights() {}
+
+  function createTextures() {
+    const textureLoader = new THREE.TextureLoader();
+  }
+
+  function createRenderer() {
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+    });
+    renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    return renderer;
+  }
+
+  function setResizeEventListeners() {
+    window.addEventListener("resize", () => {
+      // Update the sizes
+      sizes.height = window.innerHeight;
+      sizes.width = window.innerWidth;
+
+      // Update the camera
+      camera.aspect = sizes.aspectRatio;
+      camera.updateProjectionMatrix();
+
+      // Update the renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    window.addEventListener("dblclick", () => {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      } else {
+        canvas.requestFullscreen();
+      }
+    });
+  }
+
+  function createGeometries() {
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial()
+    );
+    scene.add(cube);
+  }
+}
