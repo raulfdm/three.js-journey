1cb54740192b0b34fe1ee4c7c49cc19970486237
Raul de Melo <melo.raulf@gmail.com>
2023-11-09 06:40:17 -0300

    Refactor canvas and camera animations

--- src/pages/lessons/20/script.ts
+++ src/pages/lessons/20/script.ts
@@ -26,6 +26,8 @@ function render() {
   const scene = new THREE.Scene();
 
   setResizeEventListeners();
+  setScrollEventListener();
+  setCursorEventListener();
 
   const parameters = createUiParams();
   const camera = createCamera();
@@ -46,6 +48,7 @@ function render() {
 
     renderer.render(scene, camera);
     geometries.animate(elapsedTime);
+    camera.animate();
 
     window.requestAnimationFrame(updateRenderer);
   }
@@ -55,7 +58,17 @@ function render() {
     camera.position.z = 6;
     scene.add(camera);
 
-    return camera;
+    return Object.assign(camera, {
+      animate() {
+        /**
+         * only the scrollY / the screen height would move
+         */
+        const amountToScroll =
+          (parameters.scrollY / sizes.height) * parameters.materialDistance;
+
+        camera.position.y = -amountToScroll;
+      },
+    });
   }
 
   function createTextures() {
@@ -95,17 +108,11 @@ function render() {
       renderer.setSize(sizes.width, sizes.height);
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     });
-
-    window.addEventListener("dblclick", () => {
-      if (document.fullscreenElement) {
-        document.exitFullscreen();
-      } else {
-        canvas.requestFullscreen();
-      }
-    });
   }
 
   function createGeometries() {
+    const baseX = 2;
+
     const sharedMaterial = new THREE.MeshToonMaterial({
       gradientMap: textures.gradientTexture,
       color: parameters.materialColor,
@@ -119,19 +126,22 @@ function render() {
       new THREE.TorusGeometry(1, 0.4, 16, 60),
       sharedMaterial
     );
-    mesh1.position.y = parameters.materialDistance * 0;
+    mesh1.position.y = -parameters.materialDistance * 0;
+    mesh1.position.x = -baseX;
 
     const mesh2 = new THREE.Mesh(
       new THREE.ConeGeometry(1, 2, 32),
       sharedMaterial
     );
-    mesh2.position.y = parameters.materialDistance * 1;
+    mesh2.position.y = -parameters.materialDistance * 1;
+    mesh2.position.x = baseX;
 
     const mesh3 = new THREE.Mesh(
       new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
       sharedMaterial
     );
-    mesh3.position.y = parameters.materialDistance * 2;
+    mesh3.position.y = -parameters.materialDistance * 2;
+    mesh3.position.x = -baseX;
 
     scene.add(mesh1, mesh2, mesh3);
 
@@ -156,6 +166,11 @@ function render() {
     const params = {
       materialColor: "#ffeded",
       materialDistance: 4,
+      scrollY: window.scrollY,
+      cursor: {
+        x: 0,
+        y: 0,
+      },
     };
 
     const colorHandlers = gui.addColor(params, "materialColor");
@@ -185,5 +200,21 @@ function render() {
     directionalLight.position.set(1, 1, 0);
 
     scene.add(directionalLight);
+  }
+
+  function setScrollEventListener() {
+    window.addEventListener("scroll", () => {
+      parameters.scrollY = window.scrollY;
+    });
+  }
+
+  function setCursorEventListener() {
+    window.addEventListener("mousemove", (event) => {
+      /**
+       * -0.5 make to sure to from -0.5 to 0.5 (1 unit)
+       */
+      parameters.cursor.x = event.clientX / sizes.width - 0.5;
+      parameters.cursor.y = event.clientY / sizes.height - 0.5;
+    });
   }
 }
